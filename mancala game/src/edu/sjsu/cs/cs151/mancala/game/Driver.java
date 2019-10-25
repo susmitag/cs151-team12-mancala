72189ca59ad4156fe1a076406d6179d44dff71e1
Aidan Jones <arjones239@protonmail.ch>
2019-10-25 16:24:31 -0700

    Fixed imports

--- mancala game/src/edu/sjsu/cs/cs151/mancala/game/Driver.java
+++ mancala game/src/edu/sjsu/cs/cs151/mancala/game/Driver.java
@@ -1,16 +1,15 @@
 package edu.sjsu.cs.cs151.mancala.game;
 
 import edu.sjsu.cs.cs151.mancala.model.*;
+import edu.sjsu.cs.cs151.mancala.controller.*;
+import edu.sjsu.cs.cs151.mancala.view.*;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		Game game = Game.getGame();
-		
 		Board board = game.getBoard();
 		Player player1 = new Player(board.getPlayer1Store());
 		Player player2 = new Player(board.getPlayer2Store());
-		
 	}
-
 }
