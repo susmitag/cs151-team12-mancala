be067af6e7ae49ab4b6d70656b9ab79c594b8e21
Aidan Jones <arjones239@protonmail.ch>
2019-10-25 15:29:51 -0700

    Updated MancalaException comments

--- mancala game/src/edu/sjsu/cs/cs151/mancala/MancalaException.java
+++ mancala game/src/edu/sjsu/cs/cs151/mancala/MancalaException.java
@@ -1,6 +1,14 @@
 package edu.sjsu.cs.cs151.mancala;
 
+/**
+ * Generic exception for the Mancala game
+ */
 public class MancalaException extends Exception {
+	
+	/**
+	 * Default constructor with error message
+	 * @param msg error message
+	 */
 	public MancalaException(String msg) {
 		super(msg);
 	}
