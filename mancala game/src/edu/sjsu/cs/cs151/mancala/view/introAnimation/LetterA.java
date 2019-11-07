17aed8744da97dc7688e7abb2009d0a8d306be8d
susmitag <susmita.goswami@sjsu.edu>
2019-11-06 20:02:31 -0800

    Updated letter A to include a middle dash

--- mancala game/src/edu/sjsu/cs/cs151/mancala/view/introAnimation/LetterA.java
+++ mancala game/src/edu/sjsu/cs/cs151/mancala/view/introAnimation/LetterA.java
@@ -27,14 +27,19 @@ public class LetterA implements MoveableShape {
         Point2D.Double bright = new Point2D.Double(x + width, y + height);
         Point2D.Double tleft = new Point2D.Double(x + (width / 3), y);
         Point2D.Double tright = new Point2D.Double((x + width) - (width / 3), y);
+        Point2D.Double mleft = new Point2D.Double(x + (width / 6), y + height/2);
+        Point2D.Double mright = new Point2D.Double((x + width) - (width / 6), y + height/2);
+
 
         Line2D.Double left = new Line2D.Double(bleft, tleft);
         Line2D.Double right = new Line2D.Double(bright, tright);
         Line2D.Double tmid = new Line2D.Double(tleft, tright);
+        Line2D.Double mmid = new Line2D.Double(mleft, mright);
 
         g.draw(left);
         g.draw(right);
         g.draw(tmid);
+        g.draw(mmid);
     }
 
 	public int getY() {
