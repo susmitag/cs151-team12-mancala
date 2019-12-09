e5ba87f8e9f98fce83e54ef17392173341d592e7
Richard Tumaneng <ren.tumaneng@gmail.com>
2019-12-09 03:07:43 -0800

    Update Server.java

--- mancala game/src/edu/sjsu/cs/cs151/mancala/network/Server.java
+++ mancala game/src/edu/sjsu/cs/cs151/mancala/network/Server.java
@@ -1,7 +1,10 @@
-package edu.sjsu.cs.cs151.mancala;
+package edu.sjsu.cs.cs151.mancala.networkTester;
 
 import java.applet.*;
 import java.util.Scanner;
+
+import javax.swing.JFrame;
+
 import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
@@ -12,113 +15,105 @@ import java.lang.Thread;
  * Manages the connection between two players in the server
  * Currently incomplete due to GUI and network problems needed to solve first
  */
-public class Server implements Runnable{
+public class Server{
     private final String DEFAULT_IP ="localhost";
-    private final int DEFAULT_PORT = 22222;
+    private final int DEFAULT_PORT = 51734;
 
     private String ip = DEFAULT_IP;
-    private int port = DEFAULT_PORT
-    private Scanner scanner = new Scanner(System.in);
+    private int port = DEFAULT_PORT;
+    private Scanner sc = new Scanner(System.in);
 
-    static Socket socket;
-    static DataOutputStream out;
-    static DataInputStream in;
 
     static ServerSocket serverSocket;
 
     private boolean done = false;
+    private boolean running = true;
+    private int numPlayers;
+
+    private ServerSideConnection player1;
+    private ServerSideConnection player2;
+
+    public Server(){
+        System.out.println("Starting server...");
+        numPlayers = 0;
+        try {
+            serverSocket = new ServerSocket(51734); 
+        } 
+        catch (IOException e) {
+            System.out.println("IOException from Server()");
+        }
+        System.out.println("Server started...");
+    }
+
+    public void acceptConnection(){
+        try {
+            System.out.println("Waiting for connections...");
+            while (numPlayers < 2){
+                Socket s = serverSocket.accept();
+                numPlayers++;
+                System.out.println("Player # "+ numPlayers + " have connected");
+                ServerSideConnection ssc = new ServerSideConnection(s, numPlayers);
+                if(numPlayers == 1){
+                    player1 = ssc;
+                }
+                else{
+                    player2 = ssc;
+                }
+                Thread t = new Thread(ssc);
+                t.start();
+            }
+        } 
+        catch (IOException e) {
+            System.out.println("IOException from acceptConnection()");
+        }
+    }
+
+    private class ServerSideConnection implements Runnable{
+        private Socket socket;
+        private DataOutputStream out;
+        private DataInputStream in;
+        private int playerID;
+
+        public ServerSideConnection(Socket s, int id){
+            socket = s;
+            playerID = id;
+            try {
+                in = new DataInputStream(socket.getInputStream());
+                out = new DataOutputStream(socket.getOutputStream());
+            }
+            catch (IOException e) {
+                System.out.println("IOException from ServerSideConnection()");
+            }
+        }
+
+        @Override
+        public void run(){
+            try {
+                out.writeInt(playerID);
+                out.flush();
+
+                while(running){
+
+                }
+            } 
+            catch (IOException e) {
+                System.out.println("IOException from run() ServerSideConnection()");
+            }
+        }
+    }
 
     public static void main(String[] args)throws Exception{
-        System.out.println("Starting server...");
-        serverSocket = new ServerSocket(7777); 
-        System.out.println("Server started...");
-        
-        while(true){
-            socket = serverSocket.accept();
-
-            for(int i = 0; i < 2; i++){
-                System.out.println("Connection from "+ socket.getInetAddress());
-                out = new DataOutputStream(socket.getOutputStream());
-                in = new DataInputStream(socket.getInputStream());
-               if(user[i] == null){
-                    user[i] = new Users(out,in,user);
-                    Thread thread = new Thread(user[i]);
-                    thread.start();
-                    break;
-                }
-                //out.writeUTF("This is a test of java socket");
-                //System.out.println("Data Sent");
-            }  
-        }
+        Server server = new Server();
+        server.acceptConnection();
     }
 
-    static Users[] user = new Users[2];
-    class Users implements Runnable{
-        DataOutputStream out;
-        DataInputStream in;
-        Users[] user = new Users[2];
-    
-        public Users(DataOutputStream out, DataInputStream in, Users[] user){
-            this.out = out;
-            this.in = in;
-            this.user = user;
-    
-        }
-        public void run(){
-            while (true){
-                
-            }
-        }
-    }
-    @Override
-    public void run() {
-        while(true){
-            try{    
-                String message = in.readUTF();
-                for(int i = 0; i<2; i++){
-                    if(user[i] != null){
-                        user[i].out.writeUTF(message);
-                    }
-                }
-            }
-            catch(IOException e){
-                this.out = null;
-                this.in = null;
-            }
-        }
-    }
-    public Server() throws IOException {
-        
-        System.out.println("Please input the IP: ");
-        ip = scanner.nextLine();
-        int ipAddress = Integer.parseInt(ip);
-        System.out.println("Please input the port");
-        port = scanner.nextInt();
-        while(port < 1 | port > 65535){
-            System.out.println("Please input a valid port");
-            port = scanner.nextInt();
-        }
-    }
-    public void close() {
-		try {
-			in.close();
-			out.close();
-            socket.close();
-            serverSocket.close();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		done = true;
-	}
-	
-    public void setHost(String host) {
-		this.host = host;
+    public void setHost(String ip) {
+		this.ip = ip;
 	}
 	
 	public void setPort(int port) {
 		this.port = port;
-	}
+    }
 }
     
     
